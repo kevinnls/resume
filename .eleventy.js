92b138ba19c6d8ca2c4df52d74180fe80480660a
kevinnls <developer@kevinnlsamuel.com>
2022-08-23 22:23:27 +0530

    exclude /test in production/build

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ const yaml = require('js-yaml')
 module.exports = function(eleventyConfig){
 	eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents))
 	eleventyConfig.addDataExtension("yml", contents => yaml.load(contents))
+	if (process.env.ENVIRONMENT == 'production') {
+		eleventyConfig.ignores.add('test.md')
+	}
 	return {
 		dir: {
 			layouts: '_layouts'
