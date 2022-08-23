f88fd8b700af18571d7bc794e04b4d6d48955173
kevinnls <developer@kevinnlsamuel.com>
2022-08-23 21:18:35 +0530

    create global site data; enable yaml data in config
    because my layouts depend on a site value

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
-module.exports = function(){
+const yaml = require('js-yaml')
+
+module.exports = function(eleventyConfig){
+	eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents))
+	eleventyConfig.addDataExtension("yml", contents => yaml.load(contents))
 	return {
 		dir: {
 			layouts: '_layouts'
