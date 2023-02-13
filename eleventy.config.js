e8eae5da50d3be931116fe96033033310805e6d3
kevinnls <developer@kevinnlsamuel.com>
2023-02-14 01:05:54 +0530

    (modules): js-yaml and cutom data format

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
-
+const yaml = require('js-yaml');
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addDataExtension("yml, yaml", c => yaml.load(c));
+
   return {
     dir: {
       input: 'src'
