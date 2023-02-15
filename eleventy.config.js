b4dc046abbfa2266f74f4f0c5a0ba624be71de8d
kevinnls <developer@kevinnlsamuel.com>
2023-02-16 00:21:07 +0530

    trial setup of custom filter for sorting by date

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
+const { sortbydate } = require('./lib/sortbydate.cjs');
 const { inspect } = require('node:util');
 const yaml = require('js-yaml');
 
@@ -5,6 +6,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter(
 	  "debug", (content) => `<pre>${inspect(content)}</pre>`);
+  eleventyConfig.addFilter(
+	  "sortbydate", sortbydate);
 
   eleventyConfig.addDataExtension(
 	  "yml, yaml", c => yaml.load(c));
